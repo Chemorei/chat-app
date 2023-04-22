665a74d311c0c57608665c80317c2936f0c731f5
Chemorei <chemoreidaniel@gmail.com>
2023-04-22 18:01:32 +0300

    first commit

---
+++ app/src/main/java/com/example/mychatapp/VerificationActivity.kt
@@ -0,0 +1,31 @@
+package com.example.mychatapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.renderscript.ScriptGroup.Binding
+
+class VerificationActivity : AppCompatActivity() {
+    var binding = ActivityVerificationBinding? = null
+    var auth:FirebaseAuth? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityVerificationBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+
+        auth = FirebaseAuth.getInstance()
+        if (auth.currentUser != null){
+            val intent =Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }
+        supportActionBar!!.hide()
+        binding!!.editNumber.requestFocus()
+        binding!!.continueBtn.setOnClickListener {
+            val intent = Intent(this,OTPActivity::class.java)
+            intent.putExtra("phonenumber",binding!!.editNumber.text.toString())
+            startActivity(intent)
+        }
+    }
+}
